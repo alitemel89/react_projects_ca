b1888e132380876efbb574b69338b57eabb77529
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-12-18 15:45:03 +0300

    update App.js

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, name, info, price, image }) => {
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name}/>
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+      </footer>
+    </article>
+  )
 };
 
 export default Tour;
