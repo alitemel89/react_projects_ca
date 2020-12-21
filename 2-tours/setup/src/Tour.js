ab7b9cfc6ce26d2ce42a4ce63bc52e4b2a8bda13
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-12-21 10:50:17 +0300

    update Tour.js

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -9,6 +9,8 @@ const Tour = ({ id, name, info, price, image }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
+        <p>{info}</p>
+        <button className="delete-btn">Not Interested</button>
       </footer>
     </article>
   )
