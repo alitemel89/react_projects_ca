90a0361ffa79ba1833f47c6cd94e837d74a94eb0
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-12-24 11:16:20 +0300

    add clearInterval

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -17,6 +17,13 @@ function App() {
     }
   },[index, people])
 
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    },3000);
+    return () => clearInterval(slider);
+  }, [index])
+
   return (
     <section className="section">
       <div className="title">
