13c859d2bd8756e83a4dcd65fe378bdda41a640e
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2020-12-24 11:12:55 +0300

    add useEffect

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -7,6 +7,16 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0)
 
+  useEffect(() => {
+    const lastIndex = people.length - 1
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  },[index, people])
+
   return (
     <section className="section">
       <div className="title">
@@ -18,23 +28,30 @@ function App() {
           {
             people.map((person, personIndex) => {
               const { id, image, name, title, quote } = person
-
+              let position = 'nextSlide'
+              if (personIndex === index) {
+                position = 'activeSlide';
+              }
+              if (personIndex === index - 1 || 
+                (index === 0 && personIndex === people.length - 1)) {
+                position = 'lastSlide'
+              }
               return (
-                <article key={id}>
+                <article className={position} key={id}>
                   <img src={image} alt={name} className="person-img" />
                   <h4>{name}</h4>
                   <p className="title">{title}</p>
                   <p className="text">{quote}</p>
                   <FaQuoteRight className="icon" />
                 </article>
-              )
+              );
 
             })
           }
-          <button className="prev">
+          <button onClick={() => {setIndex(index - 1)}} className="prev">
             <FiChevronLeft />
           </button>
-          <button className="next">
+          <button onClick={() => {setIndex(index + 1)}} className="next">
             <FiChevronRight />
           </button>
       </div>
